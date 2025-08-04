d39f4adc49d21e1f06e0ea8183ad20cfc864891b
Howard Beck <howard.beck314@gmail.com>
2025-08-04 02:06:49 +0200

    updating from mit.edu/~hbeck due to graduating

---
+++ webutil/template.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.removeChild(document.getElementById("js-warning"))
+
+    txt = "<br />"
+    txt += '<a href="../">return to home page</a><br /><br />'
+    txt += '<a href="https://accessibility.mit.edu/">Accessibility</a>'
+
+    div = document.createElement("div")
+    div.innerHTML = txt
+
+    document.body.appendChild(div)
+})
+
+navs = ["about", "notes", "shenanigans"]
+
+function iam(thenav) {
+    for (i = 0; i < navs.length; i++) {
+        elm = null;
+        if (thenav == navs[i]) {
+            elm = document.createElement("span")
+        } else {
+            elm = document.createElement("a")
+            href = "./"
+            if (navs[i] != "about") {
+                href += navs[i]
+            }
+
+            if (thenav != "about") {
+                href = "." + href
+            }
+            elm.href = href
+        }
+        elm.innerHTML = navs[i]
+
+        document.getElementById("nav").appendChild(elm)
+    }
+}
